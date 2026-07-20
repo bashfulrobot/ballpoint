caedda8f8cc1bf5faae968238c650d99ca252ad2
Dustin Krysak <dustin@bashfulrobot.com>
2026-07-20 16:00:04 -0700

    build: add flake devShell with go, gopls, golangci-lint, delve

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "ballpoint: Todoist triage freshness probe, triage walk, and work dispatcher";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ go gopls golangci-lint delve ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
